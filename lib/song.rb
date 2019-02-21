2293cc433044f654e2e0b7197f250d058113e2ec
Kat Maldon <katharinamaldon@gmail.com>
2019-02-21 20:18:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,13 +18,11 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #def self.reset_all
+  #  self.all.clear
+  #end
 
-  def self.count
-    self.all.count
-  end
+  
 
   def artist=(artist)
     @artist = artist
