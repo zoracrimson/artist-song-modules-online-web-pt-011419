4122dd41c0acca49ac542d7cb47de133c76a416c
Kat Maldon <katharinamaldon@gmail.com>
2019-02-21 20:29:00 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Artist
+  
+  extend Memorable
   attr_accessor :name
   attr_reader :songs
 
@@ -17,14 +19,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,11 @@
+module Memorable 
+
+  def count
+    all.count
+  end
+  
+  def reset_all
+    all.clear
+  end
+
+end 
